2f050c540d5f73efdb3ade1c18c8113cf8b3ed43
jos8304 <jyou8304@gmail.com>
2024-11-09 21:28:44 -0500

    setup for vercel

--- joblist/settings.py
+++ joblist/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -134,3 +134,4 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
